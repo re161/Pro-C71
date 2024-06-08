835c5f12bbc3e53f5a21d24edef0c6af29d1359d
re161 <77871348+re161@users.noreply.github.com>
2024-06-08 13:51:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,16 @@
 import firebase from "firebase";
 require("@firebase/firestore");
 
-//Paste your firebaseConfig here
 
-//
+var firebaseConfig = {
+  apiKey: "AIzaSyCGN0qa6gu0xEVyKY4ItwhZMbWr-8frCDc",
+  authDomain: "e-ride-46a4e.firebaseapp.com",
+  projectId: "e-ride-46a4e",
+  storageBucket: "e-ride-46a4e.appspot.com",
+  messagingSenderId: "598592373857",
+  appId: "1:598592373857:web:e60e519b5b475620055620"
+};
+
 firebase.initializeApp(firebaseConfig);
 
 export default firebase.firestore();
